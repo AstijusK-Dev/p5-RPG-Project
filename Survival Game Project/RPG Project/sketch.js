0ad5de7a7d97e902abb531834fe86c2dd8410301
Astijus Karvauskas <44985303+AstijusK-Dev@users.noreply.github.com>
2019-03-02 20:20:46 +0000

    Add files via upload

--- Survival Game Project/RPG Project/sketch.js
+++ Survival Game Project/RPG Project/sketch.js
@@ -1,60 +1,63 @@
 var player;
-var tile;
+var cell;
 var world;
 
-var renderWidth = 800;
-var renderHeight = 600;
+var renderWidth = 1024;
+var renderHeight = 768;
 
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(renderWidth, renderHeight);
 
   player = new Player();
-  tile = new Tile();
+  cell = new Cell();
   world = new World();
 }
 
 function draw() {
   background(51);
   
-  player.draw();
+  player.show();
   player.update();
   world.show();
 }
 
+function Cell(){
+  this.cellWidth = 32;
+  this.cellHeight = 32;
+}
+
+function World(){
+  this.colCells = renderWidth / 32;
+  this.rowCells = renderHeight / 32;
+
+  this.generate = function(){
+        
+  }
+  this.show = function(){
+    for(var x = 0; x < this.colCells; x++){
+      for(var y = 0; y < this.rowCells; y++){
+        var i = x*cell.cellWidth;
+        var j = y*cell.cellHeight;
+        stroke(0);
+        fill(255);
+        rect(i, j, cell.cellWidth, cell.cellHeight);
+      }
+    }
+  }
+}
+
 function Player(){
-  this.width = 64;
-  this.height = 64;
+  this.width = 32;
+  this.height = 32;
   this.x = width / 2 - (this.width / 2);
   this.y = height / 2 - (this.height / 2);
   this.moveSpeed = 3;
   
   this.update = function(){
-    
+    //Player movement controls
   }
-  this.draw = function(){
+  this.show = function(){
     fill(255);
     rect(this.x, this.y, this.width, this.height);
-  }
-}
-
-function Tile(){
-  this.width = 64;
-  this.height = 64;
-
-  this.show = function(){
-
-  }
-}
-
-function World(){
-  this.widthInTiles = renderWidth / tile.width;
-  this.heightInTiles = renderHeight / tile.height;
-
-  this.show = function(){
-    for(var tileX = 0; tileX < world.widthInTiles; tileX++){
-      for(var tileY = 0; tileY < world.heightInTiles; tileY++){
-        var newTile = new Tile();
-      }
-    }
   }
 }
